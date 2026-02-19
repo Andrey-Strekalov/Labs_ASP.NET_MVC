16e8f161120c78d3b0e23f74b24f08c9718e0c23
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2026-02-19 18:15:19 +0300

    Lab_1:individual-task

---
+++ ASP.NET_MVC_LABs/ASP.NET_MVC_LABs/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.NET_MVC_LABs.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string ISBN { get; set; } // уникальный идентификатор
+    }
+}
